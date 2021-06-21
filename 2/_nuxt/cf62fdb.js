f3a362a514c73bd0da8968c5db42081da1b2c3be
Lil Frãuder <81760000+lilfrauder@users.noreply.github.com>
2021-06-21 12:15:43 +0300

    Add files via upload

---
+++ 2/_nuxt/cf62fdb.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{236:function(e,n,t){"use strict";t.r(n);var l={name:"Skills"},c=t(38),component=Object(c.a)(l,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,"8fadafd2",null);n.default=component.exports}}]);
